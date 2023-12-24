84ac0f4482d0319ac55cccea18aaf9ba6af50582
xirang-husir <3308321550@qq.com>
2023-12-24 11:33:05 +0800

    Competition-Work

--- src/views/user/UserAvatar.vue
+++ src/views/user/UserAvatar.vue
@@ -24,7 +24,9 @@ const onUpdateAvatar = async () => {
   await userStore.getUser()
   // 提示用户
   ElMessage.success('头像更新成功')
+  console.log('头像更新成功')
 }
+
 const loading = ref(true)
 // 模拟加载效果
 onMounted(() => {
